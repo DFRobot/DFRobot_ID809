87cc8582d5563ffc2979c3522628a7e0c8241168
LZY1586 <53321447+LZY1586@users.noreply.github.com>
2020-07-03 15:36:25 +0800

    Update fingerprintDeletion.ino

--- examples/fingerprintDeletion/fingerprintDeletion.ino
+++ examples/fingerprintDeletion/fingerprintDeletion.ino
@@ -1,19 +1,17 @@
 /*!
  * @file fingerprintDeletion.ino
- * @brief Delete specific fingerprint 
- * @n Experiment phenomenon：check if the ID has been registered, delete it if it is 
-                Prompt "ID is unregistered" if it is not 
+ * @brief Delete a specific fingerprint 
+ * @n Experiment phenomenon：press your finger on the sensor, if this fingerprint is registered, delete it and LED turns on in green. 
+      If it is unregistered or fingerprint collection fails, LED light turns on in red.
  * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @licence     The MIT License (MIT)
  * @author [Eddard](Eddard.liu@dfrobot.com)
  * @version  V1.0
  * @date  2020-03-19
  * @get from https://www.dfrobot.com
- * @url https://github.com/cdjq/DFRobot_ID809
+ * @url https://github.com/DFRobot/DFRobot_ID809
 */
 #include <DFRobot_ID809.h>
-
-#define FINGERPRINTID 1   //The fingerprint ID to be deleted 
 
 /*Use software serial when using UNO or NANO */
 #if ((defined ARDUINO_AVR_UNO) || (defined ARDUINO_AVR_NANO))
@@ -49,18 +47,62 @@ void setup(){
 }
 
 void loop(){
-  /*Check whether the fingerprint ID has been registered 
+  uint8_t ret = 0;
+  Serial.println("Press your finger on the sensor to delete the fingerprint");
+  /*Set fingerprint LED ring mode, color, and number of blinks 
+    Can be set as follows: 
+    Parameter 1:<LEDMode>
+    eBreathing   eFastBlink   eKeepsOn    eNormalClose
+    eFadeIn      eFadeOut     eSlowBlink   
+    Parameter 2:<LEDColor>
+    eLEDGreen  eLEDRed      eLEDYellow   eLEDBlue
+    eLEDCyan   eLEDMagenta  eLEDWhite
+    Parameter 3:<Number of blinks> 0 represents blinking all the time 
+    This parameter will only be valid in mode eBreathing, eFastBlink, eSlowBlink
+   */
+  fingerprint.ctrlLED(/*LEDMode = */fingerprint.eBreathing, /*LEDColor = */fingerprint.eLEDBlue, /*blinkCount = */0);
+  /*Capture fingerprint image, 10s idle timeout 
+    If succeed return 0, otherwise return ERR_ID809
+   */
+  if((fingerprint.collectionFingerprint(/*timeout=*/10)) != ERR_ID809){
+    /*Compare the captured fingerprint with all the fingerprints in the fingerprint library 
+      Return fingerprint ID(1-80) if succeed, return 0 when failed 
+     */
+    ret = fingerprint.search();
+    if(ret != 0){
+      /*Delete the fingerprint of this ID*/
+      fingerprint.delFingerprint(/*Fingerprint ID = */ret);
+      //fingerprint.delFingerprint(DELALL);  //Delete all fingerprints 
+      Serial.print("delete succeeds,ID=");
+      Serial.println(ret);
+      /*Set fingerprint LED ring to always ON in green */
+      fingerprint.ctrlLED(/*LEDMode = */fingerprint.eKeepsOn, /*LEDColor = */fingerprint.eLEDGreen, /*blinkCount = */0);
+    }else{
+      Serial.println("Fingerprint is unregistered");
+      /*Set fingerprint LED ring to always ON in red*/
+      fingerprint.ctrlLED(/*LEDMode = */fingerprint.eKeepsOn, /*LEDColor = */fingerprint.eLEDRed, /*blinkCount = */0);
+    }
+  }else{
+    Serial.println("Capturing fails");
+    /*Get error code information*/
+    desc = fingerprint.getErrorDescription();
+    Serial.println(desc);
+    /*Set fingerprint LED ring to always ON in red*/
+    fingerprint.ctrlLED(/*LEDMode = */fingerprint.eKeepsOn, /*LEDColor = */fingerprint.eLEDRed, /*blinkCount = */0);
+  }
+  Serial.println("Please release your finger");
+  /*Wait for finger to release
+    Return 1 when finger is detected, otherwise return 0 
+   */
+  while(fingerprint.detectFinger());
+    /*Check whether the fingerprint ID has been registered 
     Return 1 if registered, otherwise return 0 
    */
-  if(fingerprint.getStatusID(/*Fingerprint ID = */FINGERPRINTID)){
-    Serial.println("ID has been registered");
-    /*Delete the fingerprint of this ID*/
-    fingerprint.delFingerprint(/*Fingerprint ID = */FINGERPRINTID);
-    //fingerprint.delFingerprint(DELALL);  //Delete all fingerprints 
-    Serial.println("ID has been deleted");
-  }else{
-    Serial.println("ID is unregistered");
-  }
+//  if(fingerprint.getStatusID(/*Fingerprint ID = */ret)){
+//    Serial.println("ID has been registered");
+//  }else{
+//    Serial.println("ID is unregistered");
+//  }
   Serial.println("-----------------------------");
   delay(1000);
 }
